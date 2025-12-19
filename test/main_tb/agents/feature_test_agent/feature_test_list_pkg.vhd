47237e05b65b54fbc17e361e18584bd97e93551e
Ondrej Ille <ondrej.ille@gmail.com>
2025-12-19 17:35:21 +0100

    test: add txt buffer read access feature test to extend code coverage.

--- test/main_tb/agents/feature_test_agent/feature_test_list_pkg.vhd
+++ test/main_tb/agents/feature_test_agent/feature_test_list_pkg.vhd
@@ -243,6 +243,7 @@ use ctu_can_fd_tb.tx_status_ftest.all;
 use ctu_can_fd_tb.timestamp_low_high_ftest.all;
 use ctu_can_fd_tb.txt_buffer_byte_access_ftest.all;
 use ctu_can_fd_tb.txt_buffer_hazard_ftest.all;
+use ctu_can_fd_tb.txt_buffer_read_access_ftest.all;
 use ctu_can_fd_tb.txt_buffer_transitions_ftest.all;
 use ctu_can_fd_tb.txt_buffer_transitions_2_ftest.all;
 use ctu_can_fd_tb.txt_buffer_transitions_3_ftest.all;
@@ -566,6 +567,8 @@ package body feature_test_list_pkg is
             txt_buffer_byte_access_ftest_exec(channel);
         elsif (test_name = "txt_buffer_hazard") then
             txt_buffer_hazard_ftest_exec(channel);
+        elsif (test_name = "txt_buffer_read_access") then
+            txt_buffer_read_access_ftest_exec(channel);
         elsif (test_name = "txt_buffer_transitions") then
             txt_buffer_transitions_ftest_exec(channel);
         elsif (test_name = "txt_buffer_transitions_2") then
