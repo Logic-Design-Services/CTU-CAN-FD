2a673b3ab0970a108ef1085756fa1a348f693a3f
Ondrej Ille <ondrej.ille@gmail.com>
2025-12-23 22:45:51 +0100

    test: add DBT MAXIMAL feature test to get full toggle coverage on SSP generator.

--- test/main_tb/agents/feature_test_agent/feature_test_list_pkg.vhd
+++ test/main_tb/agents/feature_test_agent/feature_test_list_pkg.vhd
@@ -110,6 +110,7 @@ use ctu_can_fd_tb.command_rrb_ftest.all;
 use ctu_can_fd_tb.counters_toggle_ftest.all;
 
 use ctu_can_fd_tb.device_id_ftest.all;
+use ctu_can_fd_tb.dbt_maximal_ftest.all;
 use ctu_can_fd_tb.disable_in_tx_ftest.all;
 use ctu_can_fd_tb.dlc_can20_8_64_bytes_ftest.all;
 
@@ -314,6 +315,8 @@ package body feature_test_list_pkg is
 
         elsif (test_name = "device_id") then
             device_id_ftest_exec(channel);
+        elsif (test_name = "dbt_maximal") then
+            dbt_maximal_ftest_exec(channel);
         elsif (test_name = "disable_in_tx") then
             disable_in_tx_ftest_exec(channel);
         elsif (test_name = "dlc_can20_8_64_bytes") then
