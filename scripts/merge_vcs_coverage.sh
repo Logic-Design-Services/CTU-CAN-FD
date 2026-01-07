c046f0ef0c1a73c4047c4e4146350e9514e9063a
ondrej.ille <ondrej.ille@gmail.com>
2026-01-07 13:49:33 +0100

    scripts,tests: adapt scripts for coverage merging. Add 2.7 big config exclude file.

--- scripts/merge_vcs_coverage.sh
+++ scripts/merge_vcs_coverage.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
-ts_sim_coverage.py -o merged_medium_asic_max_feature sim_tb_rtl_medium_asic_max_feature_* --clear
-ts_sim_coverage.py -o merged_medium_asic_typ_feature sim_tb_rtl_medium_asic_typ_feature_* --clear
-ts_sim_coverage.py -o merged_medium_asic_max_compliance sim_tb_rtl_medium_asic_max_compliance_* --clear
-ts_sim_coverage.py -o merged_medium_asic_typ_compliance sim_tb_rtl_medium_asic_typ_compliance_* --clear
-ts_sim_coverage.py -o merged_medium_asic_min_compliane sim_tb_rtl_medium_asic_min_compliance_* --clear
-ts_sim_coverage.py -o merged_medium_asic_sjw0_compliane sim_tb_rtl_medium_asic_sjw0_compliance_* --clear
+DUT_CFG=big
 
-rm -rf merged_medium_asic_max_feature
-rm -rf merged_medium_asic_typ_feature
-rm -rf merged_medium_asic_max_compliance
-rm -rf merged_medium_asic_typ_compliance
-rm -rf merged_medium_asic_min_compliane
-rm -rf merged_medium_asic_sjw0_compliane
+ts_sim_coverage.py -o merged_${DUT_CFG}_asic_max_feature sim_tb_rtl_${DUT_CFG}_asic_max_feature__* --clear
+ts_sim_coverage.py -o merged_${DUT_CFG}_asic_typ_feature sim_tb_rtl_${DUT_CFG}_asic_typ_feature__* --clear
+ts_sim_coverage.py -o merged_${DUT_CFG}_asic_max_compliance sim_tb_rtl_${DUT_CFG}_asic_max_compliance__* --clear
+ts_sim_coverage.py -o merged_${DUT_CFG}_asic_typ_compliance sim_tb_rtl_${DUT_CFG}_asic_typ_compliance__* --clear
+ts_sim_coverage.py -o merged_${DUT_CFG}_asic_min_compliane sim_tb_rtl_${DUT_CFG}_asic_min_compliance__* --clear
+ts_sim_coverage.py -o merged_${DUT_CFG}_asic_sjw0_compliane sim_tb_rtl_${DUT_CFG}_asic_sjw0_compliance__* --clear
 
-mkdir merged_medium_asic_max_feature
-mkdir merged_medium_asic_typ_feature
-mkdir merged_medium_asic_max_compliance
-mkdir merged_medium_asic_typ_compliance
-mkdir merged_medium_asic_min_compliane
-mkdir merged_medium_asic_sjw0_compliane
+rm -rf merged_${DUT_CFG}_asic_max_feature
+rm -rf merged_${DUT_CFG}_asic_typ_feature
+rm -rf merged_${DUT_CFG}_asic_max_compliance
+rm -rf merged_${DUT_CFG}_asic_typ_compliance
+rm -rf merged_${DUT_CFG}_asic_min_compliane
+rm -rf merged_${DUT_CFG}_asic_sjw0_compliane
 
-mv ../coverage_output/merged_medium_asic_max_feature.vdb merged_medium_asic_max_feature/simv.vdb
-mv ../coverage_output/merged_medium_asic_typ_feature.vdb merged_medium_asic_typ_feature/simv.vdb
-mv ../coverage_output/merged_medium_asic_max_compliance.vdb merged_medium_asic_max_compliance/simv.vdb
-mv ../coverage_output/merged_medium_asic_typ_compliance.vdb merged_medium_asic_typ_compliance/simv.vdb
-mv ../coverage_output/merged_medium_asic_min_compliane.vdb merged_medium_asic_min_compliane/simv.vdb
-mv ../coverage_output/merged_medium_asic_sjw0_compliane.vdb merged_medium_asic_sjw0_compliane/simv.vdb
+mkdir merged_${DUT_CFG}_asic_max_feature
+mkdir merged_${DUT_CFG}_asic_typ_feature
+mkdir merged_${DUT_CFG}_asic_max_compliance
+mkdir merged_${DUT_CFG}_asic_typ_compliance
+mkdir merged_${DUT_CFG}_asic_min_compliane
+mkdir merged_${DUT_CFG}_asic_sjw0_compliane
 
-ts_sim_coverage.py merged_medium_asic_max_feature merged_medium_asic_typ_feature merged_medium_asic_max_compliance \
-                   merged_medium_asic_typ_compliance merged_medium_asic_min_compliane merged_medium_asic_sjw0_compliane \
-                   -o merged_total --clear
+mv ../coverage_output/merged_${DUT_CFG}_asic_max_feature.vdb merged_${DUT_CFG}_asic_max_feature/simv.vdb
+mv ../coverage_output/merged_${DUT_CFG}_asic_typ_feature.vdb merged_${DUT_CFG}_asic_typ_feature/simv.vdb
+mv ../coverage_output/merged_${DUT_CFG}_asic_max_compliance.vdb merged_${DUT_CFG}_asic_max_compliance/simv.vdb
+mv ../coverage_output/merged_${DUT_CFG}_asic_typ_compliance.vdb merged_${DUT_CFG}_asic_typ_compliance/simv.vdb
+mv ../coverage_output/merged_${DUT_CFG}_asic_min_compliane.vdb merged_${DUT_CFG}_asic_min_compliane/simv.vdb
+mv ../coverage_output/merged_${DUT_CFG}_asic_sjw0_compliane.vdb merged_${DUT_CFG}_asic_sjw0_compliane/simv.vdb
+
+ts_sim_coverage.py merged_${DUT_CFG}_asic_max_feature merged_${DUT_CFG}_asic_typ_feature merged_${DUT_CFG}_asic_max_compliance \
+                   merged_${DUT_CFG}_asic_typ_compliance merged_${DUT_CFG}_asic_min_compliane merged_${DUT_CFG}_asic_sjw0_compliane \
+                   -o merged_${DUT_CFG}_total --clear
